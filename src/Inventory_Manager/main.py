55ab86750752e15bf120861046f09a7e9c241fbc
PrithviCode1082 <prithvikulahalli@gmail.com>
2026-05-19 21:21:52 +0530

    Test Commit

--- src/Inventory_Manager/main.py
+++ src/Inventory_Manager/main.py
@@ -1,8 +1,5 @@
 from database import inventory_data
 from utils import printInventory
 
-
 def menu():
-    print(
-        "1 - Add Supplies\n2 - Move Supplies to Shop\n3 - Check Supplies\n4 - And new Aisle\n5 - Exit"
-    )
+    print("1 - Add Supplies\n2 - Move Supplies to Shop\n3 - Check Supplies\n4 - And new Aisle\n5 - Exit")
